24e098a3e85e947ac3d31fce7b600d71b06e9ca2
AritraLahiri <aritralahiri17@gmail.com>
2020-01-23 11:28:33 +0530

    fixed env var problem

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ authRoute		 = 	require('./routes/Auth');
 
 
 // mongoose.connect('mongodb+srv://aritra:GSf6li1FuXTev75y@cluster0-3ts3r.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:false });
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.connect(process.env.DATABASE_URL || process.env.HEROKU_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 // APP CONFIG
 app.set('view engine', '.ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
