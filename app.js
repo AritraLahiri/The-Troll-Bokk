c237b677a2d07d648fb568020c7f8b9e41ebb295
AritraLahiri <aritralahiri17@gmail.com>
2020-01-23 15:45:59 +0530

    add media query

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //REQUIRING DEPENDENCIES
-// require('dotenv').config()
+require('dotenv').config()
 const express = require('express'),
 app 			 = 	express(),
 bodyParser 		 = 	require('body-parser'),
